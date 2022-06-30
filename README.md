4c2204e5bf5e2bd147041ac687e08360eb847510
Jamin Wan <jamin.wan@countdown.co.nz>
2022-06-30 20:31:01 +1200

    1.add select col duplicated row 2.add psw for the app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # streamlit
+It's a data profile tool which provides basic data profile info for snowflake table. 
--- dashboard.py
+++ dashboard.py
@@ -1,70 +1,82 @@
 import streamlit as st
 import snowflake.connector
 import pandas as pd
-# from credentials import sf_credentials
+from app_password import check_password
 
-@st.experimental_singleton
-def init_connection():
-    return snowflake.connector.connect(**st.secrets["snowflake"],insecure_mode=True, ocsp_fail_open=False)   
-con = init_connection()
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    @st.experimental_singleton
+    def init_connection():
+        return snowflake.connector.connect(**st.secrets["snowflake"],insecure_mode=True, ocsp_fail_open=False)   
+    con = init_connection()
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-# @st.experimental_memo(ttl=600)
-# def run_query(query):
-#     with con.cursor() as cur:
-#         cur.execute(query)
-#         return cur.fetch_pandas_all()
-st.title("❄️Data Profiling Dashboard")
-st.write("#### ❄️Table Address:")
+    # Perform query.
+    # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+    # @st.experimental_memo(ttl=600)
+    # def run_query(query):
+    #     with con.cursor() as cur:
+    #         cur.execute(query)
+    #         return cur.fetch_pandas_all()
+    st.title("❄️Data Profiling Dashboard")
+    st.write("#### ❄️Table Address:")
 
-db_name = st.text_input("Enter Snowflake Database Name", "JAMIN_TEST", type = "default")
-schema_name = st.text_input("Enter Snowflake Schema Name", "PUBLIC", type = "default")
-table_name = st.text_input("Enter Snowflake Table Name", "SALES", type = "default")
+    db_name = st.text_input("Enter Snowflake Database Name", "JAMIN_TEST", type = "default")
+    schema_name = st.text_input("Enter Snowflake Schema Name", "PUBLIC", type = "default")
+    table_name = st.text_input("Enter Snowflake Table Name", "SALES", type = "default")
 
-query_table_name = f"{db_name}.{schema_name}.{table_name}"
+    query_table_name = f"{db_name}.{schema_name}.{table_name}"
 
-if query_table_name:
-    # sf_table = "JAMIN_TEST.PUBLIC.SALES_TALEND_CREATE"
-    # df = run_query(query)
-    query = f"SELECT * FROM {query_table_name}"
-    query_text = f'<p style="font-family:sans-serif; color:Green; font-size: 15px;">Executed Query: {query}</p>'
-    st.markdown(query_text, unsafe_allow_html=True)
-    # exception for inputing wrong table name
-    try:
-        df = pd.read_sql(query, con)
-    except Exception as e:
-        st.warning("Entered wrong table")
-        st.stop()
-   
-    # building streamlit app
-    st.write("#### Table Preview:")
-    st.write(df.head())
+    if query_table_name:
+        # sf_table = "JAMIN_TEST.PUBLIC.SALES_TALEND_CREATE"
+        # df = run_query(query)
+        query = f"SELECT * FROM {query_table_name}"
+        query_text = f'<p style="font-family:sans-serif; color:Green; font-size: 15px;">Executed Query: {query}</p>'
+        st.markdown(query_text, unsafe_allow_html=True)
+        # exception for inputing wrong table name
+        try:
+            df = pd.read_sql(query, con)
+        except Exception as e:
+            st.warning("Entered wrong table")
+            st.stop()
+    
+        # building streamlit app
+        st.write("#### Table Preview:")
+        st.write(df.head())
 
-    # length of the table
-    columns_length = len(df.columns)
-    row_length = len(df)
+        # length of the table
+        columns_length = len(df.columns)
+        row_length = len(df)
 
-    # shape
-    st.write(f"Table has {columns_length} columns * {row_length} rows")
+        # shape
+        st.write(f"Table has {columns_length} columns * {row_length} rows")
 
-    # table status
-    st.write("#### Table Status")
-    st.write(df.describe(include="all").fillna("").astype("str"))
+        # table status
+        st.write("#### Table Status")
+        st.write(df.describe(include="all").fillna("").astype("str"))
 
-    st.write("#### Null Value")
-    st.write(df.isnull().sum())
+        st.write("#### Null Value")
+        st.write(df.isnull().sum())
 
-    st.write("#### Duplicate Row")
-    st.write("Table has", df.duplicated().sum(), " duplicated rows")
-    st.write(df[df.duplicated()])
+        st.write("#### Duplicate Row")
+        st.write("Table has", df.duplicated().sum(), " duplicated rows")
+        st.write(df[df.duplicated()])
 
+        # duplicate row based on column selections
+        st.write("#### Duplicate Row By Selecting Columns")
+        dropdown_selection = df.columns
+        #create st.form to reduce page refreshes every time user select a column
+        with st.form("choose_column"):
+            options = st.multiselect("Select Columns or Keys: ", dropdown_selection)
+            submitted = st.form_submit_button("Submit")
+        if submitted:
+            if options:
+                st.write("Based on selected columns:", ", ".join(options), ". Below is the duplicated rows:")
+                st.write(df[df.duplicated(subset=options, keep=False)])
+        # distribution graph
+        st.write("#### Distribution Graph for All Categorical Cols")
+        col_list = df.select_dtypes(include="object_").columns
+        for col in col_list:
+            st.bar_chart(df[col].value_counts())
 
-    # distribution graph
-    st.write("#### Distribution Graph for All Categorical Cols")
-    col_list = df.select_dtypes(include="object_").columns
-    for col in col_list:
-        st.bar_chart(df[col].value_counts())
-
-else:
-    st.warning('Please Input a Valid Table Name')
+    else:
+        st.warning('Please Input a Valid Table Name')
